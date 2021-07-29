baf3d435d55fd540453f13cf19e0eccbcf3661cd
Sergio <sergio.eneide@protonmail.com>
2021-07-29 20:07:51 +0100

    4th test passed

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -19,38 +19,58 @@ describe('Calculator', () => {
   });
 
   it('should add two numbers together', () => {
-  // Writing const buttons required for the test
+  // Writing const for buttons required for the test
   const button4 = container.find('#number4')
   const button1 = container.find('#number1')
   const add = container.find('#operator_add')
   const equals = container.find('#operator-equals')
   // The container finds the running total
   const runningTotal = container.find('#running-total');
-  // Adding click button simulation
+  // Adding click buttons simulation
   button4.simulate('click');
   add.simulate('click');
   button1.simulate('click');   
   equals.simulate('click');
-  // After simulating clicking the buttons it should return 5
+  // After simulating clicking the buttons, the total expected should be 5
   expect(runningTotal.text()).toEqual('5');
 });
 
 
 it('should subtract one number from another', () => {
-  // Writing const buttons required for the test
+  // Writing const for buttons required for the test
   const button7 = container.find('#number7')
   const button4 = container.find('#number4')
   const subtract = container.find('#operator-subtract');
   const equals = container.find('#operator-equals')
   // The container finds the running total
   const runningTotal = container.find('#running-total');
-  // Adding click button simulation
+  // Adding click buttons simulation
   button7.simulate('click');
   subtract.simulate('click');
   button4.simulate('click');   
   equals.simulate('click');
-  // After simulating clicking the buttons it should return 3
+  // After simulating clicking the buttons, the total expected should be 3
   expect(runningTotal.text()).toEqual('3');
 });
+
+
+  it('should multiply two numbers together', () => {
+    // Writing const for buttons required for the test
+    const button5 = container.find('#number5')
+    const button3 = container.find('#number3')
+    const multiply = container.find('#operator-multiply');
+    const equals = container.find('#operator-equals')
+    // The container finds the running total
+    const runningTotal = container.find('#running-total');
+    // Adding click buttons simulation
+    button5.simulate('click')
+    multiply.simulate('click')
+    button3.simulate('click')
+    equals.simulate('click')
+    // After simulating clicking the buttons, the total expected should be 15
+    expect(runningTotal.text()).toEqual('15');
+  });
+
+
 
 })
