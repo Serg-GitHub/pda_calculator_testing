9bb6020b6082cfc0f794351c36ef4871012dbf3e
Sergio <sergio.eneide@protonmail.com>
2021-07-29 19:50:57 +0100

    second test passed

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -26,13 +26,31 @@ describe('Calculator', () => {
   const equals = container.find('#operator-equals')
   // The container finds the running total
   const runningTotal = container.find('#running-total');
-  // Adding click function
+  // Adding click button simulation
   button4.simulate('click');
   add.simulate('click');
   button1.simulate('click');   
   equals.simulate('click');
-  // After simulating clicking the buttons it should return return 5
+  // After simulating clicking the buttons it should return 5
   expect(runningTotal.text()).toEqual('5');
+});
+
+
+it('should subtract one number from another', () => {
+  // Writing const buttons required for the test
+  const button7 = container.find('#number7')
+  const button4 = container.find('#number4')
+  const subtract = container.find('#operator-subtract');
+  const equals = container.find('#operator-equals')
+  // The container finds the running total
+  const runningTotal = container.find('#running-total');
+  // Adding click button simulation
+  button7.simulate('click');
+  subtract.simulate('click');
+  button4.simulate('click');   
+  equals.simulate('click');
+  // After simulating clicking the buttons it should return 3
+  expect(runningTotal.text()).toEqual('3');
 });
 
 })
